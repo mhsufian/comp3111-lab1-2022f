48bf60b21418949903f6b95d168ef1562e0273f2
Suf42 <sufianmohammad36@gmail.com>
2022-09-16 11:09:32 +0800

    I have made a change

--- lib/src/main/java/lab1/Library.java
+++ lib/src/main/java/lab1/Library.java
@@ -6,7 +6,7 @@ public class Library {
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
-			System.out.println("Hello, World!");
+			System.out.println("Hello, World!!!");
 	}
 		
 	public boolean someLibraryMethod() {
